3ad99912419c10224a9edf01d02d47bdd0dba671
Jorge Aguilar <georgeaguilar11@hotmail.com>
2026-03-24 18:45:50 -0600

    feat: lazy loading added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './presentation/components/Header'
-import PokemonList from './presentation/pages/PokemonList'
-import PokemonDetail from './presentation/pages/PokemonDetail'
+import PageLoader from './presentation/components/ui/PageLoader'
 import useDarkMode from './presentation/hooks/useDarkMode'
+
+const PokemonList = lazy(() => import('./presentation/pages/PokemonList'))
+const PokemonDetail = lazy(() => import('./presentation/pages/PokemonDetail'))
 
 function App() {
   const { isDark, toggle } = useDarkMode()
@@ -12,10 +15,12 @@ function App() {
       <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
         <Header isDark={isDark} onToggleDark={toggle} />
         <main>
-          <Routes>
-            <Route path="/" element={<PokemonList />} />
-            <Route path="/pokemon/:name" element={<PokemonDetail />} />
-          </Routes>
+          <Suspense fallback={<PageLoader />}>
+            <Routes>
+              <Route path="/" element={<PokemonList />} />
+              <Route path="/pokemon/:name" element={<PokemonDetail />} />
+            </Routes>
+          </Suspense>
         </main>
       </div>
     </BrowserRouter>
---
+++ src/presentation/components/ui/PageLoader.tsx
@@ -0,0 +1,9 @@
+function PageLoader() {
+  return (
+    <div className="flex justify-center items-center min-h-[60vh]">
+      <p className="text-gray-500 dark:text-gray-400 text-lg">Loading...</p>
+    </div>
+  )
+}
+
+export default PageLoader
